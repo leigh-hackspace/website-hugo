db275abd0eb192860d3498b65852371a8321fc9e
Andrew Williams <andy@tensixtyone.com>
2023-11-07 21:33:44 +0000

    Fix open status

--- themes/lhs/assets/js/site.js
+++ themes/lhs/assets/js/site.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
 
   if ($('span#hackspace-status').length) {
     $.getJSON("https://api.leighhack.org/space.json", function (data) {
-      var date = new Date(data.state.lastchange * 1000);
-
       if (data.state.open) {
         message = '<b>Open<b>'
         if ('message' in data.state) {
@@ -16,7 +14,7 @@ $(document).ready(function () {
         $('span#hackspace-status').html(message);
         $('div#hackspace-open').addClass('is-success');
       } else {
-        $('span#hackspace_status').html('<b>Closed</b>');
+        $('span#hackspace-status').html('<b>Closed</b>');
       }
     });
   }
