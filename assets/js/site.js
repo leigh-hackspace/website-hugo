1a0db21ec328bb3be2fd27136d3ce0e274492ae0
Andrew Williams <andy@tensixtyone.com>
2024-02-07 15:00:00 +0000

    Add OSM map on the Visit Us page

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,9 +2,13 @@ function titleCase(word) {
     return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
 }
 
+// SpaceAPI related calls and data
 $(document).ready(function () {
-    if ($('span#hackspace-status').length) {
-        $.getJSON("https://api.leighhack.org/space.json", function (data) {
+
+    $.getJSON("https://api.leighhack.org/space.json", function (data) {
+
+        // Hackspace status
+        if ($('span#hackspace-status').length) {
             if (data.state.open) {
                 message = '<b>Open<b>'
                 if ('message' in data.state) {
@@ -15,13 +19,11 @@ $(document).ready(function () {
             } else {
                 $('span#hackspace-status').html('<b>Closed</b>');
             }
-        });
-        console.log(atob('aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1IcDdBSVh5UkpwdyAtIERhYm8uLi4u'));
-    }
+            console.log(atob('aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1IcDdBSVh5UkpwdyAtIERhYm8uLi4u'));
+        }
 
-    // 'printers' shortcode
-    if ($('div#printer-status').length) {
-        $.getJSON("https://api.leighhack.org/space.json", function (data) {
+        // 'printers' shortcode
+        if ($('div#printer-status').length) {
             if (data.sensors.ext_3d_printers.length) {
 
                 // sort the printers
@@ -34,6 +36,42 @@ $(document).ready(function () {
                     $('div#printer-status').append(obj);
                 });
             }
-        });
-    }
+        }
+
+        // 'map' shortcode
+        if ($('div#map').length) {
+            center = ol.proj.fromLonLat([data.location.lon, data.location.lat])
+
+            // Create map
+            const map = new ol.Map({
+                target: document.getElementById('map'),
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM(),
+                    }),
+                    new ol.layer.Vector({
+                        source: new ol.source.Vector({
+                            features: [new ol.Feature({
+                                geometry: new ol.geom.Point(center),
+                                name: "Leigh Hackspace",
+                            })]
+                        }),
+                        style: new ol.style.Style({
+                            image: new ol.style.Icon({
+                              scale: .06,
+                              anchorXUnits: 'fraction',
+                              anchorYUnits: 'pixels',
+                              src: '../images/rose_logo.svg',
+                            }),
+                          }),
+                    })
+                ],
+                view: new ol.View({
+                    center: center,
+                    maxZoom: 18,
+                    zoom: 13,
+                }),
+            });
+        }
+    });
 });
