04374cdfebd3c792b8049b14bfa86f03825ef12b
Andrew Williams <andy@tensixtyone.com>
2024-02-19 14:05:14 +0000

    Add retro theme

---
+++ tools/retro-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Deploys the retro version of the website. Ran the same way as the branch deploy.
+set -u
+
+TARGET_FOLDER=$(realpath $1)
+BASE_URL="$2"
+
+# Arguments to add to the Hugo call
+HUGO_ARGUMENTS="--gc"
+
+git pull
+hugo --quiet ${HUGO_ARGUMENTS} -b "${BASE_URL}" -d "${TARGET_FOLDER}" -t "lhs-retro"
